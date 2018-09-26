49519494cd3f365edacf88314ee8be4736a33b29
Tristen Horton <tristen@tristenhorton.com>
2018-09-26 10:41:04 -0700

    support render_template replacement

--- pakk_flask/__init__.py
+++ pakk_flask/__init__.py
@@ -2,9 +2,17 @@
 Utilities for handling assets packaged in a Pakk file while working with Flask.
 """
 from mimetypes import guess_type
-from flask import send_file, send_from_directory as flask_send_from_directory
+from flask import send_file, render_template_string, send_from_directory as flask_send_from_directory, render_template as flask_render_template
 from werkzeug.exceptions import NotFound
 from pakk import Pakk
+
+class PakkTemplate:
+    """
+    An object relating a pakkfile and a template file expected to be in the pakkfile.
+    """
+    def __init__(self, pakkfile: Pakk, template_name: str):
+        self.pakkfile = pakkfile
+        self.template_name = template_name
 
 def send_from_directory(directory, filename, **options):
     """
@@ -37,5 +45,28 @@ def send_from_directory(directory, filename, **options):
             )
 
         return NotFound()
-    else:
-        return flask_send_from_directory(directory, filename, **options)
+
+    return flask_send_from_directory(directory, filename, **options)
+
+def render_template(source, **context):
+    """
+    A replacement for Flask's render_template that supports pakked files.
+
+    if source is an instance of PakkTemplate and source.template_name exists within source.pakkfile, then the pakked file is rendered.
+
+    if source is an instance of PakkTemplate and source.template_name does not exist within source.pakkfile, then source.template_name is
+    passed down to flask.render_template as source.
+
+    if source is not an instance of PakkTemplate, then this function defers to flask.render_template
+
+    """
+    if isinstance(source, PakkTemplate):
+        if source.pakkfile.has_blob(source.template_name):
+            blob = source.pakkfile.get_blob(source.template_name)
+            data = blob.get_data()
+            text = data.decode("utf-8")
+            return render_template_string(text, **context)
+
+        return flask_render_template(source.template_name, **context)
+
+    return flask_render_template(source, **context)
